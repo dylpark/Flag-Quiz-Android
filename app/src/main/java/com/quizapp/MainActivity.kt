c9899cc9bc3c092b96d0d967f6072d02ad24006a
dparksy <dylanpark.net@gmail.com>
2020-10-28 19:05:28 +1000

    Update MainActivity.kt

--- app/src/main/java/com/quizapp/MainActivity.kt
+++ app/src/main/java/com/quizapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,10 +18,22 @@ class MainActivity : AppCompatActivity() {
         // This is used to align the xml view to this class
         setContentView(R.layout.activity_main)
 
-        // TODO (STEP 6: Hide the status bar for fullscreen view.)
-        // START
         // To hide the status bar.
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-        // END
+
+        // Validate name is entered or not and launch the QuizQuestion Activity.
+        btn_start.setOnClickListener {
+
+            if (et_name.text.toString().isEmpty()) {
+
+                Toast.makeText(this@MainActivity, "Please enter your name", Toast.LENGTH_SHORT)
+                        .show()
+            } else {
+
+                val intent = Intent(this@MainActivity, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
