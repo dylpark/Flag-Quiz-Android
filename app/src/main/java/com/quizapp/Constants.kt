ee54fc48da8526785d6b6a63104d7083a0195b0f
dparksy <dylanpark.net@gmail.com>
2020-10-31 14:33:04 +1000

    Update Constants.kt

--- app/src/main/java/com/quizapp/Constants.kt
+++ app/src/main/java/com/quizapp/Constants.kt
@@ -5,6 +5,10 @@ import com.quizapp.Question
 
 object Constants {
 
+    const val USER_NAME: String = "user_name"
+    const val TOTAL_QUESTIONS: String = "total_question"
+    const val CORRECT_ANSWERS: String = "correct_answers"
+
     fun getQuestions(): ArrayList<Question> {
         val questionsList = ArrayList<Question>()
 
